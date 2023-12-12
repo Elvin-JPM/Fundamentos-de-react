217d93efde5336c02a04984ab1ea8e558cd84941
Elvin Posadas <elvinjosuep2@gmail.com>
2023-12-12 17:24:34 -0600

    some more filters

--- src/Components/Ads.js
+++ src/Components/Ads.js
@@ -6,7 +6,11 @@ function Adds({ adsList, name, minPrice, maxPrice, adType, tags }) {
   return (
     <div>
       {adsList.map((ad) =>
-        ad.name.includes(name) ? (
+        ad.name.includes(name) ||
+        Number(ad.price) >= Number(minPrice) ||
+        Number(ad.price) <= Number(maxPrice) ||
+        ad.tags === tags ||
+        (ad.sale === true ? "For sale" : "Looking to buy") === adType ? (
           <Link className={styles.link} to={`/adds/${ad.id}`} key={ad.id}>
             <div className={styles.card}>
               <p>Name: {ad.name}</p>
